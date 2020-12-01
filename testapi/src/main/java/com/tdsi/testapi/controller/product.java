1bc4c3cf0d27560347722e1e1db35449028aaee1
zertox-tech <pamodiallo@gmail.com>
2020-12-01 18:39:19 +0000

    Premier Test

---
+++ testapi/src/main/java/com/tdsi/testapi/controller/product.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.tdsi.testapi.controller;
+
+/**
+ *
+ * @author papam
+ */
+public class product {
+    private int id;
+    private String nom;
+    private int prix;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public int getPrix() {
+        return prix;
+    }
+
+    public void setPrix(int prix) {
+        this.prix = prix;
+    }
+    
+    
+}
---
+++ testapi/src/main/java/com/tdsi/testapi/model/product.java
@@ -0,0 +1,34 @@
+package com.tdsi.testapi.model;
+
+
+public class product {
+    private int id;
+    private String nom;
+    private int prix;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public int getPrix() {
+        return prix;
+    }
+
+    public void setPrix(int prix) {
+        this.prix = prix;
+    }
+    
+    
+}
